5bb15fa3064a3ead6906c66da70f667f1754d582
mestachs <smestach@bluesquare.org>
2019-12-21 20:29:17 +0100

    Updates

---
+++ precache-manifest.b8d48cac4a841c3beffe5da8fcaacdbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ec96bffcea95fba297adb5cb9146fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf85030638cf77d8e1e7",
+    "url": "/static/css/main.769a8f5c.chunk.css"
+  },
+  {
+    "revision": "f2a618636ddb8a5b39b0",
+    "url": "/static/js/2.f71ac79b.chunk.js"
+  },
+  {
+    "revision": "117f1bb9e2c56f9ad2f474d5ac578225",
+    "url": "/static/js/2.f71ac79b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf85030638cf77d8e1e7",
+    "url": "/static/js/main.977268d1.chunk.js"
+  },
+  {
+    "revision": "73f5cb982a99759765de",
+    "url": "/static/js/runtime-main.fe3ca116.js"
+  }
+]);
